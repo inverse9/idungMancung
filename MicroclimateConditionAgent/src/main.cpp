cd794bee6494797f4c0cc11e7ef0a96192ef3b1f
kdk8 <kdksantika8@gmail.com>
2024-05-14 19:56:44 +0800

    lingkungan

--- MicroclimateConditionAgent/src/main.cpp
+++ MicroclimateConditionAgent/src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
 #include <MQTT.h>
 #include <UrusanWiFi.h>
 #include <UrusanIoT.h>
 #include "secret.h"
 #include <TaskScheduler.h>
+#include <UrusanSensorLingkungan.h>
+#include <ArduinoJson.h>
+
 
 void penangkapPesan(String topic, String message);
 void task1DetailTugas();
+void subscribeTopik();
 
 UrusanWiFi urusanWiFi(ssid, pass);
 UrusanIoT urusanIoT(broker, port, id, brokerUsername, brokerPassword);
+UrusanSensorLingkungan urusanSensorLingkungan;
 Scheduler penjadwal;
 
 Task task1(3000, TASK_FOREVER, &task1DetailTugas);
@@ -19,10 +26,23 @@ void setup() {
   Serial.begin(115200);
 
   urusanWiFi.konek();
+  while(urusanWiFi.apakahKonek() == 0){
+    Serial.print(".");
+    delay(1000);
+  }
   urusanIoT.konek();
+  while(urusanIoT.apakahKonek() == 0){
+    Serial.print(".");
+    delay(1000);
+  }
+  
   urusanIoT.penangkapPesan(penangkapPesan);
-  urusanIoT.subscribe("org/IdungMancung/MicroclimateConditonAgent");
- 
+  if(urusanIoT.apakahKonek() == 1){
+    subscribeTopik();
+  }
+
+  urusanSensorLingkungan.mulai();
+
   penjadwal.init();
   penjadwal.addTask(task1);
   task1.enable();
@@ -32,23 +52,39 @@ void loop() {
   // put your main code here, to run repeatedly:
   urusanIoT.proses();
 
-  if(urusanWiFi.apakahKonek() == 1 && urusanIoT.apakahKonek() == 0){
+  if(urusanWiFi.apakahKonek() == true && urusanIoT.apakahKonek() == false){
     urusanIoT.konek();
+    if(urusanIoT.apakahKonek() == 1){
+      subscribeTopik();
+    }
   }
 
   penjadwal.execute();
 }
 
-/// @brief Fungsi callback dari fungsi subscribe objek urusanIoT
-/// @param topic 
-/// @param message 
+void subscribeTopik(){
+  urusanIoT.subscribe("tld/namaorganisasi/namadivisi/setelan");
+}
+
 void penangkapPesan(String topic, String message){
   Serial.printf("penangkapPesan: topic: %s | message: %s\n", topic.c_str(), message.c_str());
 }
 
-/// @brief Fungsi callback dari task1
 void task1DetailTugas(){
-  if(urusanIoT.apakahKonek() == 1){
-    urusanIoT.publish("org/IdungMancung", "MicroclimateConditionAgent");
+  if(urusanIoT.apakahKonek() == true){
+    if(urusanSensorLingkungan.apakahSensorSiap() == true){
+      float suhu = urusanSensorLingkungan.bacaSuhu();
+      float kelembapan = urusanSensorLingkungan.bacaKelembapan();
+
+      JsonDocument data;
+      char muatan[512];
+
+      data["suhu"] = suhu;
+      data["kelembapan"] = kelembapan;
+
+      serializeJson(data, muatan);
+
+      urusanIoT.publish("org/idungMancung,MicroclimateConditionAgent", muatan);
+    }
   }
 }
